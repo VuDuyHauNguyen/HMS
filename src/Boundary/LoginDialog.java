7296152de171ce90ddc7051522c6423ace8109e6
BeauLee2019 <blee36@student.douglascollege.ca>
2019-06-21 04:28:16 -0700

    No changes.

---
+++ src/Boundary/LoginDialog.java
@@ -0,0 +1,89 @@
+package Boundary;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import Controller.Authentication;
+import Entity.Employee;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+
+public class LoginDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	
+	private int result = Authentication.FAIL;
+
+	private static EmployeeDAOImpl employeeDAO = new EmployeeDAOImpl();
+	
+	private JTextField txtEmail;
+	private JPasswordField passwordField;
+
+	/**
+	 * Create the dialog.
+	 */
+	public LoginDialog(JFrame parent, boolean modal) {
+		super(parent, modal);
+		
+		setTitle("Login to Hospital Management System");
+		setBounds(100, 100, 300, 150);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lbEmail = new JLabel("Email");
+			lbEmail.setBounds(27, 15, 46, 14);
+			contentPanel.add(lbEmail);
+		}
+		{
+			JLabel lblPassword = new JLabel("Password");
+			lblPassword.setBounds(27, 40, 73, 14);
+			contentPanel.add(lblPassword);
+		}
+		
+		txtEmail = new JTextField();
+		txtEmail.setBounds(102, 11, 150, 20);
+		contentPanel.add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(102, 37, 150, 17);
+		contentPanel.add(passwordField);
+		{
+			JButton btnLogin = new JButton("Login");
+			btnLogin.setBounds(179, 65, 73, 23);
+			contentPanel.add(btnLogin);
+			btnLogin.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {	
+					result = Authentication.login(txtEmail.getText(), String.valueOf(passwordField.getPassword()));
+					
+					if(result == Authentication.FAIL) {
+						JOptionPane.showMessageDialog(parent, "Email does not exist or wrong password.\nPlease try again!");
+					}else {
+						setVisible(false);
+						dispose();
+					}
+				}
+			});
+			btnLogin.setActionCommand("OK");
+			getRootPane().setDefaultButton(btnLogin);
+		}
+	}
+	
+	public void showDialog() {
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		
+	}
+}
