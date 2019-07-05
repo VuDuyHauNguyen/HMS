068759399c44d881114478b5d7513c8a39df97cc
VuDuyHauNguyen <nguyenv19@student.douglascollege.ca>
2019-07-05 00:42:29 -0700

    modified AppointmentTabGUI and implemented AppointmentDAOImpl in AppointmentTabGUI to display all appointments

--- src/Boundary/GUIComponents/AppointmentTabGUI.java
+++ src/Boundary/GUIComponents/AppointmentTabGUI.java
@@ -9,28 +9,80 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 
 import com.toedter.calendar.JDateChooser;
 
+import Boundary.DAO.AppointmentDAOImpl;
+import Boundary.Helpers.GUIHelper;
+import Entity.Appointment;
+import Entity.Employee;
+
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+
 public class AppointmentTabGUI extends JPanel {
 	
-	
-	private JPanel panelManageAppointment;
-	private Object tabbedPanes;
 	private JTable tableAppointment;
-	private JTextField appmntRecptID;	
+	private JTextField receptionistIdTxtBox;	
 	private JTextField textField;
+	private JTextField patientIdTxtBox;
+	private JTextField appointmentIdTxtBox;
+	
+	private DefaultTableModel tm;
+	private ListSelectionListener lsl;
+	private AppointmentDAOImpl appointmentDAO = new AppointmentDAOImpl();
+	
 
+	private void updateTable() {
+		//remove listener
+		tableAppointment.getSelectionModel().removeListSelectionListener(lsl);
+		
+		//array of column names in the table
+		String[] columnNames = {"Id", "Receptionist Id", "Patient Id", "Date", "Time", "Status"};
+		
+		//create a DefaultTableModel object
+		tm = GUIHelper.populateTableModel(columnNames, appointmentDAO.getAllAppointments());
+		
+		tableAppointment.setModel(tm);
+		
+		tableAppointment.setRowSorter(new TableRowSorter(tm));
+		
+		//add listener
+		tableAppointment.getSelectionModel().addListSelectionListener(lsl);
+	}
+	
 	public AppointmentTabGUI() {
 		
 		setLayout(null);
+		
+		//create lsl
+		lsl = new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				// TODO Auto-generated method stub
+				int currId = (int) tableAppointment.getValueAt(tableAppointment.getSelectedRow(), 0);//1st column
+				
+				//get the appointment
+				Appointment app = appointmentDAO.getAppointmentById(currId);
+				
+//				updateCurrentAppointmentInfo(app);
+			}
+		};
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 13, 512, 354);
+		add(scrollPane);
 		tableAppointment = new JTable();
-		tableAppointment.setBounds(12, 13, 512, 354);
-		this.add(tableAppointment);
+		scrollPane.setViewportView(tableAppointment);
 			
 		JLabel lblManageAppointment = new JLabel("Manage Appointment");
 		lblManageAppointment.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblManageAppointment.setBounds(567, 10, 159, 20);
+		lblManageAppointment.setBounds(567, 13, 159, 20);
 		add(lblManageAppointment);
 			
 		JLabel lblRecptTitle = new JLabel("(This section is filled out by the receptionist only.)");
@@ -38,41 +90,77 @@ public class AppointmentTabGUI extends JPanel {
 		lblRecptTitle.setBounds(551, 36, 193, 13);
 		add(lblRecptTitle);
 			
-		JLabel lblRecptID = new JLabel("Receptionist ID:");
-		lblRecptID.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		lblRecptID.setBounds(536, 62, 78, 16);
-		add(lblRecptID);
+		JLabel lblPatientID = new JLabel("Receptionist ID:");
+		lblPatientID.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblPatientID.setBounds(536, 85, 78, 16);
+		add(lblPatientID);
 			
-		JLabel lblApptDate = new JLabel("Appointment Date");
+		JLabel lblApptDate = new JLabel("Appointment Date:");
 		lblApptDate.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		lblApptDate.setBounds(538, 119, 86, 13);
+		lblApptDate.setBounds(536, 150, 86, 13);
 		add(lblApptDate);
 			
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(636, 119, 100, 22);
-		add(dateChooser);
-			
-		JLabel lblRecptStatus = new JLabel("Status:");
-		lblRecptStatus.setBounds(558, 199, 56, 16);
-		add(lblRecptStatus);
+		JDateChooser appointmentDate = new JDateChooser();
+		appointmentDate.setBounds(628, 141, 116, 22);
+		add(appointmentDate);
 			
 		JComboBox<String> appmntRecptStatusCbox = new JComboBox<String>();
-		appmntRecptStatusCbox.setBounds(636, 196, 96, 22);
+		appmntRecptStatusCbox.setBounds(628, 210, 116, 22);
 		add(appmntRecptStatusCbox);
 			
-		JLabel lblAppmntTime = new JLabel("Time:");
-		lblAppmntTime.setBounds(558, 158, 56, 16);
-		add(lblAppmntTime);
-			
 		JComboBox appmntRecptTimeCbox = new JComboBox();
-		appmntRecptTimeCbox.setModel(new DefaultComboBoxModel(new String[] {"00:00", "00:15", "00:30", "00:45", "01:00", "01:15", "01:30", "01:45", "02:00", "02:15", "02:30", "02:45", "03:00", "03:15", "03:30", "03:45", "04:00", "04:15", "04:30", "04:45", "05:00", "05:15", "05:30", "05:45", "06:00", "06:15", "06:30", "06:45", "07:00", "07:15", "07:30", "07:45", "08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45", "24:00"}));
-		appmntRecptTimeCbox.setBounds(635, 157, 91, 22);
+		appmntRecptTimeCbox.setModel(new DefaultComboBoxModel(new String[] {"08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00"}));
+		appmntRecptTimeCbox.setBounds(628, 175, 116, 22);
 		add(appmntRecptTimeCbox);
 			
-		appmntRecptID = new JTextField();
-		appmntRecptID.setBounds(636, 62, 90, 22);
-		add(appmntRecptID);
-		appmntRecptID.setColumns(10);			
+		receptionistIdTxtBox = new JTextField();
+		receptionistIdTxtBox.setEditable(false);
+		receptionistIdTxtBox.setBounds(628, 79, 116, 22);
+		add(receptionistIdTxtBox);
+		receptionistIdTxtBox.setColumns(10);			
+		
+		JLabel patientIdLbl = new JLabel("Patient ID:");
+		patientIdLbl.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		patientIdLbl.setBounds(536, 116, 78, 16);
+		add(patientIdLbl);
+		
+		JLabel appointmentTimeLbl = new JLabel("Appointment Time:");
+		appointmentTimeLbl.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		appointmentTimeLbl.setBounds(536, 184, 86, 13);
+		add(appointmentTimeLbl);
+		
+		patientIdTxtBox = new JTextField();
+		patientIdTxtBox.setColumns(10);
+		patientIdTxtBox.setBounds(628, 110, 116, 22);
+		add(patientIdTxtBox);
+		
+		JLabel appointmentIdLbl = new JLabel("Appointment ID:");
+		appointmentIdLbl.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		appointmentIdLbl.setBounds(536, 56, 78, 16);
+		add(appointmentIdLbl);
+		
+		appointmentIdTxtBox = new JTextField();
+		appointmentIdTxtBox.setEditable(false);
+		appointmentIdTxtBox.setColumns(10);
+		appointmentIdTxtBox.setBounds(628, 50, 116, 22);
+		add(appointmentIdTxtBox);
+		
+		JLabel statusLbl = new JLabel("Status:");
+		statusLbl.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		statusLbl.setBounds(536, 219, 86, 13);
+		add(statusLbl);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setBounds(658, 338, 86, 29);
+		add(btnAdd);
+		
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.setBounds(628, 244, 116, 29);
+		add(btnUpdate);
+		
+		JButton btnClear = new JButton("Clear Form");
+		btnClear.setBounds(536, 338, 115, 29);
+		add(btnClear);
 		
 		JLabel lblDetails = new JLabel("Details:");
 		lblDetails.setBounds(537, 20, 56, 16);		
@@ -100,5 +188,7 @@ public class AppointmentTabGUI extends JPanel {
 			
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setBounds(647, 230, 91, 22);		
+		
+		updateTable();
 	}
 }
