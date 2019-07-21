2bb85f30bfbf5601115929d2a2e2ce9ed156843d
VuDuyHauNguyen <nguyenv19@student.douglascollege.ca>
2019-07-21 01:03:07 -0700

    added update a checkUpRecord; get a checkUpRecord by id; get a checkUpRecord by id and has status 'queue' or 'checkup'; get all checkUpRecords; get all checkUpRecords that have status 'queue' or 'checkup'

--- src/Boundary/DAO/CheckUpRecordDAOImpl.java
+++ src/Boundary/DAO/CheckUpRecordDAOImpl.java
@@ -3,10 +3,12 @@ package Boundary.DAO;
 import java.util.ArrayList;
 
 import org.hibernate.HibernateException;
+import org.hibernate.LockMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import Entity.Appointment;
 import Entity.CheckUpRecord;
 
 public class CheckUpRecordDAOImpl {
@@ -68,6 +70,136 @@ public class CheckUpRecordDAOImpl {
 		
 		//return all checkUpRecords
 		return checkUpRecords;
+	}
+	
+	//GET all records in queue or in progress
+	public ArrayList<CheckUpRecord> getAllCheckUpRecordInQueueOrInProgressRecords() {
+		//Initialize variables
+		SessionFactory fx = null;
+		Session sx = null;
+		Transaction tx = null;
+		
+		ArrayList<CheckUpRecord> checkUpRecords = null;
+		
+		try {
+			fx = HibernateFactory.getFactory();
+			sx = fx.openSession();
+			tx = sx.beginTransaction();
+			
+			//get all checkUpRecords
+			checkUpRecords = (ArrayList<CheckUpRecord>) 
+					sx.createQuery("FROM CheckUpRecord WHERE status IN (:state)")
+					.setParameterList("state", new String[] {
+							CheckUpRecord.STATUS_QUEUE,
+							CheckUpRecord.STATUS_IN_PROGRESS
+					})
+					.list(); 
+			
+		}catch(HibernateException hx) {
+			System.err.println(hx.getMessage());
+		}finally {
+			sx.close();
+			fx.close();
+		}
+		
+		//return all checkUpRecords
+		return checkUpRecords;
+	}
+	
+	//GET a record
+	public CheckUpRecord getCheckUpRecordById(int id) {
+		//Initialize variables
+		SessionFactory fx = null;
+		Session sx = null;
+		
+		CheckUpRecord checkUpRecord = null;
+		
+		try {
+			fx = HibernateFactory.getFactory();
+			sx = fx.openSession();
+			
+			//get checkUpRecord
+			checkUpRecord = sx.get(CheckUpRecord.class, id);
+			
+		}catch(HibernateException hx) {
+			System.err.println(hx.getMessage());
+		}finally {
+			sx.close();
+			fx.close();
+		}
+		
+		return checkUpRecord;
+	}
+	
+	//GET a record in queue
+	public CheckUpRecord getCheckUpRecordInQueueById(int id) {
+		//Initialize variables
+		SessionFactory fx = null;
+		Session sx = null;
+		Transaction tx = null;
+		
+		CheckUpRecord checkUpRecord = null;
+		
+		try {
+			fx = HibernateFactory.getFactory();
+			sx = fx.openSession();
+			tx = sx.beginTransaction();
+			
+			//get a checkUpRecord and lock the row
+			checkUpRecord = sx.get(CheckUpRecord.class, id, LockMode.PESSIMISTIC_WRITE);
+			
+			//check if status = 'queue'
+			if(checkUpRecord.getStatus().equals(CheckUpRecord.STATUS_QUEUE)) {
+				
+				//change status to 'checkup'
+				checkUpRecord.setStatus(CheckUpRecord.STATUS_IN_PROGRESS);
+				
+				//update database to prevent other doctor see it 
+				sx.save(checkUpRecord);
+			}else//return null
+				checkUpRecord = null;
+			
+			tx.commit();
+		}catch(HibernateException hx) {
+			if(tx != null) tx.rollback();
+			System.err.println(hx.getMessage());
+		}finally {
+			sx.close();
+			fx.close();
+		}
+		
+		return checkUpRecord;
+	}
+	
+	//UPDATE 
+	public boolean updateCheckUpRecord(CheckUpRecord checkUpRecord) {
+		//Initialize variables
+		SessionFactory fx = null;
+		Session sx = null;
+		Transaction tx = null;
+		
+		boolean result = true;
+		
+		try {
+			fx = HibernateFactory.getFactory();
+			sx = fx.openSession();
+			tx = sx.beginTransaction();
+			
+			//update an appointment
+			sx.update(checkUpRecord);
+			
+			tx.commit();
+			
+		}catch(HibernateException hx) {
+			if(tx != null) tx.rollback();
+			result = false;
+			System.err.println(hx.getMessage());
+		}finally {
+			sx.close();
+			fx.close();
+		}
+		
+		return result;
 	}
 	
 }
