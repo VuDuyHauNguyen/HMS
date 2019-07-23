fb7f40cf7e5d2ef35709d5d78a91aea3e4820275
BeauLee2019 <blee36@student.douglascollege.ca>
2019-07-23 01:41:36 -0700

    CreateAn AppointmentTest.java is successful.

--- tests/WhiteBoxTests/CreateAnAppointmentTest.java
+++ tests/WhiteBoxTests/CreateAnAppointmentTest.java
@@ -1,5 +1,28 @@
 package WhiteBoxTests;
 
-public class CreateAnAppointmentTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import Boundary.DAO.AppointmentDAOImpl;
+import Entity.Appointment;
+
+public class CreateAnAppointmentTest {
+	Appointment appt;
+	AppointmentDAOImpl apptDAO;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		apptDAO = new AppointmentDAOImpl();
+		appt = new Appointment();
+	}
+
+	@Test
+	void createAppoitnment() {		
+		apptDAO.addAppointment(appt);
+	}
+	
+	@Test
+	void checkAppoitnment() {
+		
+	}
 }
