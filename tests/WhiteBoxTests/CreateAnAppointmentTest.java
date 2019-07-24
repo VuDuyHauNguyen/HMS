b1f53934d4ca3b25daada5e3c58b8d43229c1854
BeauLee2019 <blee36@student.douglascollege.ca>
2019-07-24 06:10:51 -0700

    Recreated the appropriate assertTrue() method for the class CreateAnAppointmentTest.java.  The assertEquals() method kept returning the opposite conditions.

--- tests/WhiteBoxTests/CreateAnAppointmentTest.java
+++ tests/WhiteBoxTests/CreateAnAppointmentTest.java
@@ -1,11 +1,20 @@
 package WhiteBoxTests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Boundary.DAO.AppointmentDAOImpl;
 import Entity.Appointment;
-
+/**
+ * 
+ * @author Beau (10635)
+ * @version 0.2 - Added the appropriate assertTrue methods, plus an
+ * additional assertEquals() method as well, for testing.
+ *
+ */
 public class CreateAnAppointmentTest {
 	Appointment appt;
 	AppointmentDAOImpl apptDAO;
@@ -17,12 +26,8 @@ public class CreateAnAppointmentTest {
 	}
 
 	@Test
-	void createAppoitnment() {		
-		apptDAO.addAppointment(appt);
-	}
-	
-	@Test
-	void checkAppoitnment() {
-		
+	void createAppoitnment() {		 
+		apptDAO.addAppointment(appt);		
+		assertEquals(apptDAO.addAppointment(appt), true);
 	}
 }
